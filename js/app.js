0f42b2adf85319af477e074886b7e67edaa53b6e
Florian <florian@goeldenitz.org>
2023-06-29 18:36:56 +0200

    show data summary and zoom level

--- js/app.js
+++ js/app.js
@@ -12,30 +12,75 @@ xhr.onload = () => {
 
     fitData = xhr.response;
 
-    for (var i=0; i<fitData.length; i++) {
-        dt = Date.parse(fitData[i].Timestamp);
-        pwr = parseInt(fitData[i].Power);
+    movingTime = new Date();
+    movingTime.setTime(fitData.MovingTime);
+
+    document.getElementById("activity-date").innerHTML = fitData.Date;
+    document.getElementById("activity-product-name").innerHTML = fitData.Product;
+    document.getElementById("activity-moving-time").innerHTML = movingTime.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12:false});
+    document.getElementById("activity-distance").innerHTML = fitData.Distance/100000 + " km";
+    document.getElementById("activity-normalized-power").innerHTML = fitData.NormalizedPower + " watt";
+
+    var dtOld = 0;
+    var records = fitData.Records;
+
+    for (var i=0; i<records.length; i++) {
+
+        /* keep this to convert timestamp in record into data
+        to display time based graph set 'dt' instead of 'distance' for the x-axis
+
+        dt = Date.parse(records[i].Timestamp);
+
+        if (i==0) {
+            dtOld = dt;
+            dt = 0;
+        } else {
+            dt = dt - dtOld;
+        }
+
+        newDate = new Date();
+        newDate.setTime(dt);
+        */
+        distance = records[i].Distance/100000;
+        pwr = parseInt(records[i].Power);
         if (pwr == 65535) pwr = 0;
-        altData.push([dt, parseInt(fitData[i].Altitude)]);
-        powerData.push([dt, pwr]);
+        altData.push([distance, parseInt(records[i].Altitude)]);
+        powerData.push([distance, pwr]);
     }
  
     var powergraph = new Dygraph(
         document.getElementById("powergraph"),
-        powerData, // path to CSV file
-        {}                  
-    )
+        powerData,
+        {
+            title: "power",
+            ylabel: 'power (w)',
+            labels: [ 'Date', 'Watt' ],
+            animatedZooms: true,
+            zoomCallback: graphZoomCallback
+        }
+    );
     
 
     var altitudegraph = new Dygraph(
         document.getElementById("altitudegraph"),
-        altData, // path to CSV file
-        {}                 
+        altData, 
+        {
+            title: "elevation",
+            ylabel: "elevation (m)",
+            labels: [ 'Date', 'hm' ],
+            animatedZooms: true,
+            zoomCallback: graphZoomCallback
+        }
     );
 
     Dygraph.synchronize(altitudegraph, powergraph,
     {
         range: false
-    });
-    
+    }); 
+}
+
+function graphZoomCallback(minX, maxX, yRanges) {
+    s = document.getElementById("debug");
+    s.innerHTML = "<b>Zoom</b> selected distance: " + (maxX-minX) + "<br>";
+    //s.innerHTML += "<b>Zoom</b> [" + minX + ", " + maxX + ", [" + yRanges + "]]<br />";
 }
